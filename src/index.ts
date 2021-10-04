c33c2c947cce1b9a6c8817a3011cce953ea49d36
Petro Ivankiv <petro.ivankiv@perfectial.com>
2021-10-04 14:44:38 +0300

    chore: init commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import * as Hapi from '@hapi/hapi';
+
+const init = async () => {
+
+  const server = Hapi.server({
+    port: 8081,
+    host: 'localhost'
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
