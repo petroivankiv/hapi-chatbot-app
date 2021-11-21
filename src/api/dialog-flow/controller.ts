ddf1bea04488ac2b41592039380f399451d3ad85
Petro Ivankiv <petro.ivankiv@perfectial.com>
2021-11-21 23:00:47 +0200

    feat: added dialog flow module

--- src/api/dialog-flow/controller.ts
+++ src/api/dialog-flow/controller.ts
@@ -1,6 +1,11 @@
 import * as Hapi from '@hapi/hapi';
-import createResponse from '../../helper/response';
 import * as Boom from '@hapi/boom';
+import createResponse from '../../helper/response';
+const dialogflow = require('dialogflow');
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(process.env.GOOGLE_PROJECT_ID, process.env.DIALOG_FLOW_SESSION_ID);
 
 export default class DialogFlowController {
   constructor() {}
@@ -9,8 +14,32 @@ export default class DialogFlowController {
     const { prisma } = request.server.app;
     const { text } = request.payload as any;
 
+    // The text query request.
+    const dialogFlowRequest = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: text,
+          languageCode: process.env.DIALOG_FLOW_SESSION_LANG_CODE,
+        },
+      },
+    };
+
     try {
-      return h.response(createResponse(request, { value: { res: text || 'Text query' } }));
+      // Send request and log result
+      const responses = await sessionClient.detectIntent(dialogFlowRequest);
+      console.log('Detected intent');
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+        console.log(`  No intent matched.`);
+      }
+
+      return h.response(createResponse(request, { value: result }));
     } catch (err: any) {
       return h.response(createResponse(request, { boom: Boom.badImplementation(err) }));
     }
