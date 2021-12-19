99d0a3343ac4ab6fac564f722dccd47bb7db0b03
Petro Ivankiv <petro.ivankiv@perfectial.com>
2021-12-19 18:43:48 +0200

    feat: added api calls

--- web/src/app/dialog-flow/dialog-flow.module.ts
+++ web/src/app/dialog-flow/dialog-flow.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { DialogFlowComponent } from './dialog-flow.component';
 
 
@@ -6,6 +9,10 @@ import { DialogFlowComponent } from './dialog-flow.component';
 @NgModule({
   declarations: [
     DialogFlowComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
   ],
 })
 export class DialogFlowModule { }
--- web/src/app/dialog-flow/dialog-flow.service.ts
+++ web/src/app/dialog-flow/dialog-flow.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogFlowService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTextQuery(text: string): Observable<any> {
+    return this.http.post<any>(environment.apiUrl + '/api/text-query', { text });
+  }
+
+  getEventQuery(event: string): Observable<any> {
+    return this.http.post<any>(environment.apiUrl + '/api/event-query', { event });
+  }
 }
